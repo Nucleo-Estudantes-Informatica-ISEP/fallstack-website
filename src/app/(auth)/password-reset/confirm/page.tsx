0bd7f0bcd7c92f9f7e5b5abf31421b61d841d964
tiagoabreu22 <tiago.abreu.mail@gmail.com>
2025-11-25 20:06:33 +0000

    fix: prerendering preventing build

--- src/app/(auth)/password-reset/confirm/page.tsx
+++ src/app/(auth)/password-reset/confirm/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useRef, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { motion } from "framer-motion";
 import swal from "sweetalert";
@@ -9,7 +9,7 @@ import useSession from "@/hooks/useSession";
 import Input from "@/components/Input";
 import PrimaryButton from "@/components/PrimaryButton";
 
-const PasswordResetConfirmPage: React.FC = () => {
+const PasswordResetForm: React.FC = () => {
   const session = useSession();
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -48,7 +48,7 @@ const PasswordResetConfirmPage: React.FC = () => {
       return setPwError("A password deve conter pelo menos 8 caracteres.");
     if (!password.match(passwordRegex))
       return setPwError(
-        "A password deve conter pelo menos 1 letra maiúscula, 1 letra minúscula e 1 número."
+          "A password deve conter pelo menos 1 letra maiúscula, 1 letra minúscula e 1 número."
       );
 
     if (password !== repeatPasswordRef.current?.value)
@@ -78,9 +78,9 @@ const PasswordResetConfirmPage: React.FC = () => {
       }
 
       swal(
-        "Sucesso",
-        "Password resetada com sucesso. Volta a iniciar sessão.",
-        "success"
+          "Sucesso",
+          "Password resetada com sucesso. Volta a iniciar sessão.",
+          "success"
       );
       router.push("/login");
     } catch (e) {
@@ -95,86 +95,94 @@ const PasswordResetConfirmPage: React.FC = () => {
   };
 
   return (
-    <div className="relative max-h-[90vh] w-full overflow-hidden md:mt-4">
-      <section className="flex max-h-[85vh] flex-col overflow-y-auto">
-        <h1 className="mb-8 w-full text-center font-sans text-[24px] font-semibold text-white md:text-left md:text-[45px]">
-          Reset Password
-        </h1>
+      <div className="relative max-h-[90vh] w-full overflow-hidden md:mt-4">
+        <section className="flex max-h-[85vh] flex-col overflow-y-auto">
+          <h1 className="mb-8 w-full text-center font-sans text-[24px] font-semibold text-white md:text-left md:text-[45px]">
+            Reset Password
+          </h1>
 
-        {codeError && (
-          <motion.p
-            className="mb-3 text-sm font-bold text-red-600"
-            animate={{ y: [-15, 0] }}
-            transition={{ ease: "easeOut", duration: 0.2 }}
-          >
-            {codeError}
-          </motion.p>
-        )}
+          {codeError && (
+              <motion.p
+                  className="mb-3 text-sm font-bold text-red-600"
+                  animate={{ y: [-15, 0] }}
+                  transition={{ ease: "easeOut", duration: 0.2 }}
+              >
+                {codeError}
+              </motion.p>
+          )}
 
-        <div className="w-full">
-          <Input
-            name="Password"
-            placeholder="Insere a tua password"
-            type="password"
-            inputRef={passwordRef}
-            autoFocus={!!pwError}
-            onKeyUp={handleKeyUp}
-            className="!rounded-none !border-[rgba(255,255,255,0.35)] bg-transparent px-3 py-2 text-white placeholder:text-gray-500 sm:py-3"
-          />
-        </div>
+          <div className="w-full">
+            <Input
+                name="Password"
+                placeholder="Insere a tua password"
+                type="password"
+                inputRef={passwordRef}
+                autoFocus={!!pwError}
+                onKeyUp={handleKeyUp}
+                className="!rounded-none !border-[rgba(255,255,255,0.35)] bg-transparent px-3 py-2 text-white placeholder:text-gray-500 sm:py-3"
+            />
+          </div>
 
-        {pwError && (
-          <motion.p
-            className="mt-1 text-sm font-bold text-red-600"
-            animate={{
-              y: [-15, 0],
-            }}
-            transition={{
-              ease: "easeOut",
-              duration: 0.2,
-            }}
-          >
-            {pwError}
-          </motion.p>
-        )}
+          {pwError && (
+              <motion.p
+                  className="mt-1 text-sm font-bold text-red-600"
+                  animate={{
+                    y: [-15, 0],
+                  }}
+                  transition={{
+                    ease: "easeOut",
+                    duration: 0.2,
+                  }}
+              >
+                {pwError}
+              </motion.p>
+          )}
 
-        <div className="mt-4 w-full">
-          <Input
-            name="Repete a Password"
-            placeholder="Repete a tua password"
-            type="password"
-            inputRef={repeatPasswordRef}
-            onKeyUp={handleKeyUp}
-            className="!rounded-none !border-[rgba(255,255,255,0.35)] bg-transparent px-3 py-2 text-white placeholder:text-gray-500 sm:py-3"
-          />
-        </div>
+          <div className="mt-4 w-full">
+            <Input
+                name="Repete a Password"
+                placeholder="Repete a tua password"
+                type="password"
+                inputRef={repeatPasswordRef}
+                onKeyUp={handleKeyUp}
+                className="!rounded-none !border-[rgba(255,255,255,0.35)] bg-transparent px-3 py-2 text-white placeholder:text-gray-500 sm:py-3"
+            />
+          </div>
 
-        {repeatPwError && (
-          <motion.p
-            className="mt-1 text-sm font-bold text-red-600"
-            animate={{
-              y: [-15, 0],
-            }}
-            transition={{
-              ease: "easeOut",
-              duration: 0.2,
-            }}
-          >
-            {repeatPwError}
-          </motion.p>
-        )}
+          {repeatPwError && (
+              <motion.p
+                  className="mt-1 text-sm font-bold text-red-600"
+                  animate={{
+                    y: [-15, 0],
+                  }}
+                  transition={{
+                    ease: "easeOut",
+                    duration: 0.2,
+                  }}
+              >
+                {repeatPwError}
+              </motion.p>
+          )}
 
-        <div className="mt-6 w-full">
-          <PrimaryButton
-            loading={loading}
-            onClick={handleClick}
-            className="!flex w-full cursor-pointer !items-center !justify-center !rounded-none !bg-[#B1440A] !px-3 py-3 !text-[17px] font-semibold !tracking-normal hover:!bg-[#8d3508] sm:py-4 sm:!text-[19px]"
-          >
-            CONFIRMAR
-          </PrimaryButton>
-        </div>
-      </section>
-    </div>
+          <div className="mt-6 w-full">
+            <PrimaryButton
+                loading={loading}
+                onClick={handleClick}
+                className="!flex w-full cursor-pointer !items-center !justify-center !rounded-none !bg-[#B1440A] !px-3 py-3 !text-[17px] font-semibold !tracking-normal hover:!bg-[#8d3508] sm:py-4 sm:!text-[19px]"
+            >
+              CONFIRMAR
+            </PrimaryButton>
+          </div>
+        </section>
+      </div>
+  );
+};
+
+const PasswordResetConfirmPage: React.FC = () => {
+  return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <PasswordResetForm />
+      </Suspense>
   );
 };
 
