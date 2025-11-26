7c913c1fc40528a0420d1da5d694faab102457e8
David Valente <1241264@isep.ipp.pt>
2025-11-26 11:53:07 +0000

    Update actionId extraction logic in scan handlers
    Replaces the use of split('-')[1] with replace(/^action-/, '') for extracting actionId from scanned data in CompanySavedProfilesSection and ScanTab. This improves reliability when parsing action IDs from QR code data.

--- src/components/QRCode/QRCodeTab/ScanTab/index.tsx
+++ src/components/QRCode/QRCodeTab/ScanTab/index.tsx
@@ -25,7 +25,7 @@ const ScanTab: React.FC<ScanTabProps> = ({ setHidden }) => {
   }
 
   async function handleActionScan(data: string) {
-    const actionId = data.split("-")[1];
+    const actionId = data.replace(/^action-/, "");
 
     const res = await fetch(BASE_URL + `/actions/${actionId}`, {
       method: "POST",
