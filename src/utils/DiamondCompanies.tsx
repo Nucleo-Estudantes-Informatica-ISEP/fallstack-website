b11a86f54d2a030fffcac5db10b9b77f50c534a1
David Valente <david.valente@nei-isep.org>
2026-07-12 18:41:00 +0100

    fix(companies): escape apostrophes in diamond tier copy

--- src/utils/DiamondCompanies.tsx
+++ src/utils/DiamondCompanies.tsx
@@ -127,7 +127,7 @@ export const DiamondCompanies: CompanyProps[] = [
         <>
           <p>
             Cloudflare is on a mission to help build a better Internet. Today the company
-            runs one of the world's largest networks that powers millions of websites and
+            runs one of the world&apos;s largest networks that powers millions of websites and
             other Internet properties for customers ranging from individual bloggers to
             SMBs to Fortune 500 companies. Cloudflare protects and accelerates any Internet
             application online without adding hardware, installing software, or changing a
@@ -138,8 +138,8 @@ export const DiamondCompanies: CompanyProps[] = [
             Internet properties powered by Cloudflare all have web traffic routed through
             its intelligent global network, which gets smarter with every request. As a
             result, they see significant improvement in performance and a decrease in spam
-            and other attacks. Cloudflare was named to Entrepreneur Magazine's Top Company
-            Cultures list and ranked among the World's Most Innovative Companies by Fast
+            and other attacks. Cloudflare was named to Entrepreneur Magazine&apos;s Top Company
+            Cultures list and ranked among the World&apos;s Most Innovative Companies by Fast
             Company.
           </p>
         </>
