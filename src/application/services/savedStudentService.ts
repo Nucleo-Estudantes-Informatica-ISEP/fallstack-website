493bb7b08f687aa18e77c1da622d91d44351c6ae
David Valente <david.valente@nei-isep.org>
2026-07-13 23:35:29 +0100

    chore: fix import order in savedStudentService

--- src/application/services/savedStudentService.ts
+++ src/application/services/savedStudentService.ts
@@ -3,8 +3,8 @@ import "server-only";
 import { Email } from "@/types/Email";
 import { HttpError } from "@/types/HttpError";
 import type { Stats } from "@/types/Stats";
-import { ISEP_EMAIL_DOMAIN } from "@/utils/isepEmail";
 import { getBoothActionName } from "@/edition/actions";
+import { ISEP_EMAIL_DOMAIN } from "@/utils/isepEmail";
 
 import { assertStudentCanBeSaved } from "../domain/saveRules";
 import {
